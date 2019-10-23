fc67e8516e98f21f39bcd5eeaab33073fbc47107
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-10-22 23:30:55 -0500

    initial commit

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from datetime import datetime, timedelta
+import os
+import pytz
+
+tz = pytz.timezone('America/Panama')
+
+def img_list(request):
+
+    utc = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    pan = datetime.now()
+    pan = pytz.utc.localize(pan, is_dst=None).astimezone(tz).strftime('%Y-%m-%d %H:%M:%S')
+
+    images = os.listdir('/var/www/eq/media/screenshots')
+    images.sort(reverse=True)
+    num_events = len(images)
+
+    context = {
+        'utctime': utc,
+        'panamatime': pan,
+        'images': images,
+        'numevents': num_events,
+    }
+    return render(request, 'main/index.html', context)
